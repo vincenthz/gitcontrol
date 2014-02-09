2c05aaa13e9680afeb4fbfb79d9190c5ae492274
Vincent Hanquez <vincent@snarc.org>
2014-02-09 01:43:43 +0000

    Use pattern match instead of length and head to get the username.

--- src/GitControl.hs
+++ src/GitControl.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Monad (when)
 import Control.Monad.IO.Class (liftIO)
 
 import Data.ByteString.Char8
@@ -16,13 +15,15 @@ findIn key ((k,c):xs) = if (key == k) then Just c else findIn key xs
 
 main = do
     args <- getArgs
-    when (1 /= (Prelude.length args)) (error "invalid command line")
-    let userName = Prelude.head args
-    envs <- getEnvironment
-    case findIn "SSH_ORIGINAL_COMMAND" envs of
-        Nothing   -> Prelude.putStrLn "error, command not found"
-        Just ocmd -> do let theCmd = commandLineParser ocmd
-                        db <- liftIO $ DB.init :: IO [DB.Entity]
-                        case DB.member (\(DB.Entity _ n) -> n == userName) db of
-                            Nothing -> error $ "not authorized user: " ++ (unpack userName)
-                            Just _  -> executeFile (gitCmd theCmd) True (gitCmdArgs theCmd) (Just envs)
+    case args of
+        [user] -> authUser user
+        _      -> error "invalid command line"
+  where authUser userName = do
+            envs <- getEnvironment
+            case findIn "SSH_ORIGINAL_COMMAND" envs of
+                Nothing   -> Prelude.putStrLn "error, command not found"
+                Just ocmd -> do let theCmd = commandLineParser ocmd
+                                db <- liftIO $ DB.init :: IO [DB.Entity]
+                                case DB.member (\(DB.Entity _ n) -> n == userName) db of
+                                    Nothing -> error $ "not authorized user: " ++ (unpack userName)
+                                    Just _  -> executeFile (gitCmd theCmd) True (gitCmdArgs theCmd) (Just envs)
