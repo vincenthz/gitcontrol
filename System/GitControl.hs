22cb24e07919d668392d7e64fb0325304054c631
Vincent Hanquez <vincent@snarc.org>
2014-02-09 19:01:24 +0000

    delay dbGet and cleanup error

--- System/GitControl.hs
+++ System/GitControl.hs
@@ -19,18 +19,18 @@ import System.GitControl.Shell
 import System.GitControl.Class
 import System.GitControl.Types
 
-defaultMain :: GitControl a => (IO a) -> IO ()
+defaultMain :: GitControl a => IO a -> IO ()
 defaultMain dbGet = do
     args <- getArgs
     case args of
         [user] -> authUser user
         _      -> error "invalid command line"
   where authUser userName = do
-            db   <- dbGet
             envs <- getEnvironment
             case lookup "SSH_ORIGINAL_COMMAND" envs of
-                Nothing   -> putStrLn "error, command not found"
+                Nothing   -> error "SSH_ORIGINAL_COMMAND not found"
                 Just ocmd -> do let theCmd = commandLineParser ocmd
+                                db         <- dbGet
                                 authorized <- isAuthorized db
                                                            (Username userName)
                                                            (RepositoryPath $ head $ gitCmdArgs theCmd)
